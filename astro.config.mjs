8de7686e305a6133cdf365fb9182e361dee9b46a
prakhar-saxena01 <prakhar_saxena@outlook.com>
2024-12-13 17:14:10 +0530

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.prakhar.pro',
 	integrations: [
 		starlight({
 			title: 'Notes',
+			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 6 },			
 			sidebar: [
 				{
 					label: 'Polity',
---
+++ src/content/docs/polity/index.md
@@ -0,0 +1,5 @@
+---
+title: POLITY NOTES
+description: These are my polity notes that are comprehensive and helpful for everyone.
+---
+![image of preamble](../../../assets/preamble.svg)
